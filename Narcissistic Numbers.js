1b6e0d9824055cece9aa64b27113c9dfc56ed49d
psterritt5 <65088511+psterritt5@users.noreply.github.com>
2022-06-01 21:01:22 -0400

    Add files via upload

---
+++ Narcissistic Numbers.js
@@ -0,0 +1,14 @@
+/*A Narcissistic Number is a number of length l in which the sum of its digits to the power of l is equal to the original
+number. If this seems confusing, refer to the example below.
+
+Ex: 153, where l = 3 ( the number of digits in 153 )
+13 + 53 + 33 = 153
+
+Write a function that, given n, returns whether or not n is a Narcissistic Number.*/
+
+function isNarcissistic(num) {
+    let numSplitToArray = num.toString().split("");
+    let sumCubed = 0;
+    numSplitToArray.forEach(element => sumCubed+=(parseInt(element) ** numSplitToArray.length))
+    return sumCubed === num;
+  }
